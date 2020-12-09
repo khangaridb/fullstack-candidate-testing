6a63919c0ad3b376592c04b1b0384c866f14e95a
khangaridb <khangarid.ba@gmail.com>
2020-12-09 12:43:55 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDb } from './dbConnection';
+import routes from './controllers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const path = require('path');
+
+connectDb();
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// init routes
+app.use('/', routes);
+
+const { PORT = 3100 } = process.env;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.log(`server is running on port ${PORT}`);
+});
