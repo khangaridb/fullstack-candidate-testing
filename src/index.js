5dc714d3396a74f395f1aa0031014166d2047a22
khangaridb <khangarid.ba@gmail.com>
2020-12-09 14:08:37 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDb } from './dbConnection';
 import routes from './controllers';
+
+const express = require('express');
 
 connectDb();
 
@@ -22,5 +23,5 @@ app.use('/', routes);
 
 app.listen(process.env.PORT || 3000, () => {
   // eslint-disable-next-line
-  console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${process.env.PORT}`);
 });
